9ddd9809da3a54f9ca0f8440d6c0cf9256fe3f08
muhammmedfahim4321 <fahimmuhammedfahimkp@gmail.com>
2023-12-27 19:57:00 +0530

    change product  models

--- Backend/shop/models.py
+++ Backend/shop/models.py
@@ -108,7 +108,8 @@ class Product(BaseModel):
 
 
 class ProductVariantImages(BaseModel):
-    name      = models.CharField(max_length=200) 
+    name      = models.CharField(max_length=200,unique=True) 
+    color     = models.OneToOneField(Color,on_delete=models.CASCADE,null=True)
     img_1     = models.ImageField(upload_to="prdv1/",null=True,blank=True)
     img_2     = models.ImageField(upload_to="prdv2/",null=True,blank=True)
     img_3     = models.ImageField(upload_to="prdv3/" ,null=True,blank=True)
@@ -119,7 +120,6 @@ class ProductVariant(BaseModel):
     name      = models.CharField(max_length=200,unique=True,null=True)
     slug      = models.SlugField(max_length=200,unique=True,null=True)
     product   = models.ForeignKey(Product,on_delete=models.CASCADE)
-    color     = models.ForeignKey(Color,on_delete=models.CASCADE)
     img       = models.ForeignKey(ProductVariantImages,on_delete=models.CASCADE,default=None)
     size      = models.ForeignKey(Size,null=True,on_delete=models.CASCADE)
     price     = models.DecimalField(default=0.0,decimal_places=2,max_digits=15)
